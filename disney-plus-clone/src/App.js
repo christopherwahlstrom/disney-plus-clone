3b7403a0cf7ca52a9fef229669768a5beb3dae32
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2022-09-20 19:07:29 +0200

    update

--- disney-plus-clone/src/App.js
+++ disney-plus-clone/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
 import Detail from './components/Detail';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 
 
@@ -11,15 +11,11 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Header />
-        <Switch>
-          <Route path='/detail'>
-            <Detail />
-          </Route>
-          <Route path='/'>
-            <Home />
-          </Route>
-        </Switch>
+          <Header />
+          <Routes>
+            <Route path="/" element={ <Home />}></Route>
+            <Route path="/detail" element={ <Detail />}></Route>
+          </Routes>
       </Router>
     </div>
   );
--- disney-plus-clone/src/components/Detail.js
+++ disney-plus-clone/src/components/Detail.js
@@ -3,9 +3,11 @@ import styled from 'styled-components'
 
 function Detail() {
     return(
-        <div>
-            DETAIL 
-        </div>
+        <Container>
+            <Background>
+                <img src="https://www.periodicoelgancho.com/wp-content/uploads/2018/06/bao-el-nuevo-corto-de-disney-pixar-2.jpg" alt="pixarfullpic" />
+            </Background>
+        </Container>
     )
 }
 
@@ -14,4 +16,20 @@ export default Detail
 const Container = styled.div`
     min-height: calc(100vh - 70px);
     padding: 0 calc(3.5vw + 5px);
+    position: relative;
+`
+const Background = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
 `
